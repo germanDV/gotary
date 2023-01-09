9951b24424a8e27d9b002c4012d8a66f312359e6
German Viescas <germanviescas@gmail.com>
2023-01-09 16:06:18 -0300

    Sort contacts by name

--- frontend/src/components/Keys.jsx
+++ frontend/src/components/Keys.jsx
@@ -3,6 +3,10 @@ import { useLoaderData } from "react-router-dom"
 import Key from "./Key"
 import ImportKey from "./ImportKey"
 
+function alphabetically(a, b) {
+  return a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+}
+
 const Keys = () => {
   const { myPublicKey, contacts } = useLoaderData()
   return (
@@ -11,7 +15,7 @@ const Keys = () => {
       <Key publicKey={myPublicKey} name="Mine" me />
 
       <h4>IMPORTED</h4>
-      {contacts.map(c => (
+      {contacts.sort(alphabetically).map(c => (
         <Key key={c.publicKey.hex} publicKey={c.publicKey.hex} name={c.name} />
       ))}
       
