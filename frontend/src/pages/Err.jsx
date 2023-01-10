8906705e223a9fb8882c81f5032c33a93a617d9b
German Viescas <germanviescas@gmail.com>
2023-01-10 11:08:10 -0300

    Add back link to error page

--- frontend/src/pages/Err.jsx
+++ frontend/src/pages/Err.jsx
@@ -1,12 +1,15 @@
 import React from "react"
-import { useRouteError } from "react-router-dom"
+import { useRouteError, useNavigate } from "react-router-dom"
 
 const Err = () => {
   const err = useRouteError()
+  const navigate = useNavigate()
+
   return (
     <main>
       <h1>ERROR!</h1>
       <h4 className="error">{JSON.stringify(err)}</h4>
+      <p className="link" onClick={() => navigate(-1)}>&larr; Go Back</p>
     </main>
   )
 }
